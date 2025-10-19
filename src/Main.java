aebb6d9b10ade05c3fc279685058b7ca134f74ce
user <user@example.com>
2025-10-19 21:36:45 +0500

    fix edge cases

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Coffee coffee = new SimpleCoffee();
         System.out.println(coffee.getCoffee());
-
         System.out.println(". Total price: "+coffee.getCost());
 
         coffee = new SugarDecorator(coffee);
         System.out.println(coffee.getCoffee());
-
         System.out.println(". Total price: "+coffee.getCost());
 
         coffee = new MilkDecorator(coffee);
         System.out.println(coffee.getCoffee());
-
         System.out.println(". Total price: "+coffee.getCost());
 
 
