da0eb60c01a73a26364041d3bda4365760bbdc5e
user <user@example.com>
2025-10-19 21:59:37 +0500

    fix edge cases

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Coffee coffee = new SimpleCoffee();
         System.out.println(coffee.getCoffee());
-        System.out.println(". Total price: "+coffee.getCost());
+        System.out.println("Total price: "+coffee.getCost());
 
         coffee = new SugarDecorator(coffee);
         System.out.println(coffee.getCoffee());
-        System.out.println(". Total price: "+coffee.getCost());
+        System.out.println("Total price: "+coffee.getCost());
 
         coffee = new MilkDecorator(coffee);
         System.out.println(coffee.getCoffee());
-        System.out.println(". Total price: "+coffee.getCost());
+        System.out.println("Total price: "+coffee.getCost());
 
 
 
